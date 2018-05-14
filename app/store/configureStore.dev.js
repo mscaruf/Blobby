4761b0a5285ff0faf0c12cd6794ddb5b8274468b
Marco Scaruffi <marco.scaruffi@hotmail.it>
2018-05-14 22:26:20 +0200

    Enabling Redux devtools extension.

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -3,6 +3,7 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const history = createHistory();
 const middleware = routerMiddleware(history);
@@ -11,9 +12,8 @@ export function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        compose(
+        composeWithDevTools(
             applyMiddleware(middleware),
-            DevTools.instrument()
         )
     );
 }
