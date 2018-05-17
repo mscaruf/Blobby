584d52fc2c4241db537cd5ecc2e9eea436628812
Marco Scaruffi <marco.scaruffi@musixmatch.com>
2018-05-17 14:08:30 +0200

    Now sass classes get loaded properly.

--- webpack.config.js
+++ webpack.config.js
@@ -44,10 +44,29 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
+                loaders: ["style-loader","css-loader","sass-loader"]
             },
-            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
+            {
+                test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property
+                loader:"file-loader",
+                query:{
+                  name:'[name].[ext]',
+                  outputPath:'images/'
+                  //the images will be emmited to public/assets/images/ folder
+                  //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png);
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule
+                loader: "url-loader",
+                query:{
+                  limit:'10000',
+                  name:'[name].[ext]',
+                  outputPath:'fonts/'
+                  //the fonts will be emmited to public/assets/fonts/ folder
+                  //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }
+                }
+            },
         ]
     }
 };
