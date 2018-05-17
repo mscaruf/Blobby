69b2028746ac364ba36a6ee718cc7b903743c56b
Marco Scaruffi <marco.scaruffi@musixmatch.com>
2018-05-17 16:31:09 +0200

    Adding images asset folder / path in server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var path = require('path');
 
 new WebpackDevServer(webpack(config), {
+    contentBase: path.resolve('app/assets'),
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true,
