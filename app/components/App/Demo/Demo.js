ad02f07bab6aa6c6c4777ce55ad2f86d4b01c25d
Marco <marco@localhost.localdomain>
2018-05-27 18:17:30 +0200

    Added/Implemented react form component.

--- app/components/App/Demo/Demo.js
+++ app/components/App/Demo/Demo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as DemoActions from '../../../actions/functions/demo';
+import { Form, Text } from 'react-form';
 
 class Demo extends React.Component {
 
@@ -17,12 +18,43 @@ class Demo extends React.Component {
           );
     }
 
+    getSubmitResult = (formApi) => {
+        this.formValues = formApi.values;
+        this.formApi = formApi;
+        //this.setState({formValues: formApi.values});
+    }
+
+    outerSubmit = (e) => {
+        e.preventDefault();
+        console.log("CIAONE!");
+        debugger;
+    }
+
     render() {
+        const validate = value => ({
+            error: !value || !/Hello World/.test(value) ? "Input must contain 'Hello World'" : null,
+            warning: !value || !/^Hello World$/.test(value) ? "Input should equal just 'Hello World'" : null,
+            success: value && /Hello World/.test(value) ? "Thanks for entering 'Hello World'!" : null
+            })
+
         return (
             <div>
                 <h2>AppName:</h2>
                 <h3>{this.props.appName}</h3>
                 {this.getApiData()}
+
+                <Form>
+                {formApi => (
+                  <form onSubmit={this.getSubmitResult(formApi)} id="form1" className="mb-4">
+                    <label htmlFor="hello">Hello World</label>
+                    <Text field="hello" id="hello" validate={validate} />
+                    <button type="submit" className="btn btn-primary" onClick={this.outerSubmit}>
+                      Submit
+                    </button>
+                    {(typeof formApi.errors !== 'undefined') && <p>{formApi.errors.hello}</p>}
+                  </form>
+                )}
+                </Form>
             </div>
         );
     }
