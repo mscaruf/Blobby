24cbeff14cf90be19a0282864bdd6dc3778b0778
Marco Scaruffi <marco.scaruffi@hotmail.it>
2018-05-28 23:26:15 +0200

    Added comments to form component.

--- app/components/App/Demo/Form/DemoForm.js
+++ app/components/App/Demo/Form/DemoForm.js
@@ -18,16 +18,19 @@ class DemoForm extends React.Component {
         this.formApi = formApi;
     }
 
-    outerSubmit = (e) => {
+    submit = (e) => {
         e.preventDefault();
         if (typeof this.formApi.errors !== 'undefined') {
             return;
         }
+
+        // DatePicker gets added to form before submitting
         this.formApi.values.startDate = this.state.startDate.format("DD-MM-YYYY");
         this.props.callback(this.formApi.values);
     }
 
     render(){
+
       const validate = value => ({
           error: !value || !/Hello World/.test(value) ? "Input must contain 'Hello World'" : null,
           warning: !value || !/^Hello World$/.test(value) ? "Input should equal just 'Hello World'" : null,
@@ -35,13 +38,19 @@ class DemoForm extends React.Component {
           })
 
         return(
+
+            // all form elemnts gets wrapped inside of a variable (formApi)
+            // onSubmit key on form tag stores the whole form state in real-time
+            // external form components (like DatePicker) must be handled separately
+            // when submit button gets clicked, all form data gets passed to a callback function
+
             <Form>
             {formApi => (
               <form onSubmit={this.mapFormToComponent(formApi)} id="form1" className="mb-4">
                 <DatePicker selected={this.state.startDate} onChange={this.handleChange}/>
                 <label htmlFor="hello">Hello World</label>
                 <Text field="hello" id="hello" validate={validate} />
-                <button type="submit" className="btn btn-primary" onClick={this.outerSubmit}>
+                <button type="submit" className="btn btn-primary" onClick={this.submit}>
                   Submit
                 </button>
                 {(typeof formApi.errors !== 'undefined') && <p>{formApi.errors.hello}</p>}
