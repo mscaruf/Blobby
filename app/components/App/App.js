f5e2a172cb5e26d5266d86a7608e78c96b91946f
Marco <marco@localhost.localdomain>
2018-06-06 21:38:40 +0200

    Text for Styles sections.

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { footer } from '../../styles/global.scss';
+import * as styles from '../../styles/global.scss';
 import Routes from '../../routes';
 
 const App = () =>
     <div className="container">
         <h1>Blobby</h1>
         { Routes }
-        <footer className={footer}>
+        <footer>
             <Link to="/walkthrough/overview">Overview</Link>
             <Link to="/walkthrough/getting-started">Getting started</Link>
             <Link to="/walkthrough/folder-structure">Folder structure</Link>
